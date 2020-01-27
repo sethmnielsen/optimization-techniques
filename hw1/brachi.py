5843bcbe28f880c045f06ba8cc4a66f8d31d5f98
Seth Nielsen <sethmnielsen@gmail.com>
2020-01-27 14:36:18 -0600

    Reverted commit to not include giant hst files

--- hw1/brachi.py
+++ hw1/brachi.py
@@ -20,7 +20,7 @@ sns.set_style('whitegrid')
 
 class Minimize:
     def __init__(self):
-        self.num_pts = np.array([4, 8, 16, 32, 64, 128]) # number of pts including start and end
+        self.num_pts = np.array([4, 8, 16, 32]) # number of pts including start and end
         # self.num_pts = np.array([4])
         self.warm_start = True
         self.ms = 1.0
@@ -32,11 +32,10 @@ class Minimize:
         # Plotting histories
         self.time_hist = []
         self.wall_time_hist = []
-        self.func_calls = []
+        self.func_evals = []
         self.num_iterations = []
 
     def init_problem(self, n):
-        print(f"--------------------------------------------------------\n")
         print(f"Solving Brachistochrone problem with n = {n}...")
         
         self.n = n
@@ -74,14 +73,24 @@ class Minimize:
             self.plot_this_solution(n)
 
         self.plot_final_results()
-            
+
     def solve_problem(self, opt_prob: pyop.Optimization, optimizer: pyop.SNOPT):
         sol: Solution = optimizer(opt_prob, storeHistory=f"opt_hist{self.n}.hst")
-
+        
         print("...done!")
-        print(f"Printing solution for n = {self.n}:", sol)
-
+        
+        sol.fStar *= np.sqrt(2/9.81)
+        print(f'sol.fStar: {sol.fStar}')
+        print(f'self.test: {self.test}')
+        print(f'self.test2: {self.test2}')
+        
         self.y_arr[1:-1] = sol.xStar['y']
+        self.time_hist.append(sol.fStar)
+        self.wall_time_hist.append(sol.optTime)
+        self.func_evals.append(sol.userObjCalls)
+        print("sol.optTime:", sol.optTime)
+        print("Calls to objective function:", sol.userObjCalls)
+        print(f"Printing solution for n = {self.n}:", sol)
     
     def objfunc(self, dvars):
         g = 9.81
@@ -94,6 +103,7 @@ class Minimize:
         y[0] = h 
 
         time_sum = 0
+        time_sum2 = 0
         for i in range(self.n-1):
             # Loop over x and y arrays
             xi = self.x_arr[i]
@@ -103,14 +113,18 @@ class Minimize:
             dx = xip-xi
             dy = yip-yi
             
-            a = np.sqrt(2/g)
+            # Gravity not needed - will multiply it for final result 
+            a = np.sqrt(2.0/g)
             b = np.sqrt(dx**2+dy**2) 
             c = np.sqrt(h-yip-mu*xip) + np.sqrt(h-yi-mu*xi)
 
-            time_sum += a * b/c
+            time_sum += b/c
+            time_sum2 += a*b/c
 
         funcs['obj'] = time_sum
         fail = False
+        self.test = time_sum * a
+        self.test2 = time_sum2
         return funcs, fail
     
     def plot_this_solution(self, n):
@@ -127,10 +141,22 @@ class Minimize:
         self.ax_pts.set_xlabel('x')
         self.ax_pts.set_ylabel('y')
         self.ax_pts.legend(title='No. Points')
-        plt.show()
 
-        # fig_time = plt.figure()
-        # ax_time = fig_time.add_subplot(111)
+        fig, axes = plt.subplots(nrows=3, ncols=1, sharex=True)
+        # plt.gcf().subplots_adjust(left=0.15)
+        
+        axes[0].plot(self.num_pts, self.time_hist)
+        axes[0].set_title("Dimensionality")
+        axes[0].set_ylabel("travel time (s)")
+        
+        axes[1].plot(self.num_pts, self.wall_time_hist)
+        axes[1].set_ylabel("wall\ntime (s)")
+        
+        axes[2].plot(self.num_pts, self.func_evals)
+        axes[2].set_ylabel("function\nevaluations")
+        axes[2].set_xlabel('number of points')
+        
+        plt.show()
 
 if __name__ == '__main__':
     op = Minimize()
