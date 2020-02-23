f97f4fbc971bd38ca4fb83ead5aa5edeb4eab49b
Seth Nielsen <sethmnielsen@gmail.com>
2020-02-23 02:16:14 -0700

    Auto-pushed changes

--- hw3/derivatives.py
+++ hw3/derivatives.py
@@ -65,47 +65,61 @@ def adjoint(A:ndarray):
 
 def relative_errors(dm_arr, ds_arr, dm_true, ds_true):
     ''' SHAPES
-        dm_arr:  (3,10)
-        ds_arr:  (3,10,10)
-        dm_true: (1,10)
-        ds_true: (1,10,10)
+        dm_arr:  (3,p,n)
+        ds_arr:  (3,p,n,n)
+        dm_true: (1,p,n)
+        ds_true: (1,p,n,n)
 
         OUT SHAPES - averages
-        errors_mass:   (10)
-        errors_stress: (10,10)
+        errors_mass:   (3,p,n)
+        errors_stress: (3,p,n,n)
     '''
-    errors_mass = np.mean( (dm_arr - dm_true)/dm_true, axis=1 )
-    errors_stress = np.mean( (ds_arr - ds_true)/ds_true, axis=1 )
+    errors_mass = (dm_arr - dm_true)/dm_true
+    errors_stress = (ds_arr - ds_true)/ds_true
 
     return errors_mass, errors_stress
 
 
 if __name__ == '__main__':
-    A_list = np.zeros((10,10))
-    A_list[0] = np.ones(10)
-    A_list[1:] = np.random.randn(9,10)
     methods = ['FD', 'complex', 'AD', 'adjoint']
+    n = 10  # number of truss elements
+    p = 100 # number of trials
+    # rng = np.random.default_rng()
+    # A_list = rng.uniform(0.5, 1.5, size=(p,n))
+    p = 1
+    A_list = np.ones((p,n))
 
-    dm_arr = np.zeros((4,10,10))
-    ds_arr = np.zeros((4,10,10,10))
+    dm_arr = np.zeros((4,p,n))
+    ds_arr = np.zeros((4,p,n,n))
+    m_arr = np.zeros((4,p))
+    s_arr = np.zeros((4,p,n))
     for i, method in enumerate(methods):
         for j, A in enumerate(A_list):
             m, s, dm, ds = get_derivatives(method, A)
             dm_arr[i,j] = dm
             ds_arr[i,j] = ds
+            m_arr[i,j] = m
+            s_arr[i,j] = s
 
     em, es = relative_errors(dm_arr[:-1], ds_arr[:-1], dm_arr[-1], ds_arr[-1])
 
-    dm_avg = np.mean(dm_arr, axis=1)
-    ds_avg = np.mean(ds_arr, axis=1)
-
     # np.set_printoptions(linewidth=200, precision=20)
-    np.set_printoptions(precision=6, linewidth=200, floatmode='maxprec', suppress=False)
+    np.set_printoptions(precision=6, linewidth=200, floatmode='fixed', suppress=False)
+    print(f'\nmass avg: {np.mean(m_arr)}, max: {np.max(m_arr)}')
+    print(f'stress avg: {np.mean(s_arr)}, max: {np.max(s_arr)}')
+    print(f'\ndm avg: {np.mean(dm_arr)}, max: {np.max(dm_arr)}')
+    print(f'ds avg: {np.mean(ds_arr)}, max: {np.max(ds_arr)}')
     print(f'\n-----MEAN VALUES-----')
-    print(f'\n\nerrors_mass:\n{em}')
-    print(f'\nerrors_stress:\n{es}')
+    # print(f'\nmean error mass:\n{em_mean}')
+    # print(f'\nmean error stress:\n{es_mean}')
+    print(f'\nmean mass error for each method: {np.mean(em,axis=(1,2))}')
+    print(f'mean stress error for each method: {np.mean(es,axis=(1,2,3))}')
 
-    # print(f'\nmasses: {m_np_avg}')
-    # print(f'\nstress arrays: {s_np_avg}')
-    print(f'\n\n\ndm: \n{dm_avg}')
-    print(f'\n\nds: \n{ds_avg}')
+    print(f'\n\n-----MAX VALUES-----')
+    # print(f'\nmax error mass:\n{em_max}')
+    # print(f'\nmax error stress:\n{es_max}')
+    print(f'\nmax mass error for each method: {np.max(em,axis=(1,2))}')
+    print(f'max stress error for each method: {np.max(es,axis=(1,2,3))}\n')
+
+    # print(f'\n\n\ndm: \n{dm_avg}')
+    # print(f'\n\nds: \n{ds_avg}')
